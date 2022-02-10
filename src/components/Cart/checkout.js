9c003f60c9ee151bf24d1cdc95f536a86d9ace67
hosseint14 <htavasol.ht@gmail.com>
2022-02-10 11:16:04 +0330

    first commit

---
+++ src/components/Cart/checkout.js
@@ -0,0 +1,101 @@
+import { useRef, useState } from "react";
+import classes from "./checkout.module.css";
+
+const isEmpty = (value) => value.trim().length === 0;
+const lowerThanFiveChar = (value) => value.trim().length < 5;
+
+const Checkout = (props) => {
+  const [formValidity, setFormValidity] = useState({
+    name: true,
+    street: true,
+    postalCode: true,
+    city: true,
+  });
+
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+    const nameInput = nameInputRef.current.value;
+    const streetInput = streetInputRef.current.value;
+    const postalCodeInput = postalCodeInputRef.current.value;
+    const cityInput = cityInputRef.current.value;
+
+    const nameIsValid = !isEmpty(nameInput);
+    const streetIsValid = !isEmpty(streetInput);
+    const postalCodeIsValid = !lowerThanFiveChar(postalCodeInput);
+    const cityIsValid = !isEmpty(cityInput);
+
+    setFormValidity({
+      name: nameIsValid,
+      street: streetIsValid,
+      postalCode: postalCodeIsValid,
+      city: cityIsValid,
+    });
+
+    const formIsValid =
+      nameIsValid && streetIsValid && postalCodeIsValid && cityIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+    props.onConfirm({
+      name: nameInput,
+      street: streetInput,
+      postalCode: postalCodeInput,
+      city: cityInput,
+    });
+  };
+
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div
+        className={`${classes.control} ${
+          formValidity.name ? "" : classes.invalid
+        }`}
+      >
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" ref={nameInputRef} />
+        {!formValidity.name && <p>please enter valid name!</p>}
+      </div>
+      <div
+        className={`${classes.control} ${
+          formValidity.street ? "" : classes.invalid
+        }`}
+      >
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" ref={streetInputRef} />
+        {!formValidity.street && <p>please enter valid street!</p>}
+      </div>
+      <div
+        className={`${classes.control} ${
+          formValidity.postalCode ? "" : classes.invalid
+        }`}
+      >
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal" ref={postalCodeInputRef} />
+        {!formValidity.postalCode && <p>please enter valid postal code!</p>}
+      </div>
+      <div
+        className={`${classes.control} ${
+          formValidity.city ? "" : classes.invalid
+        }`}
+      >
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" ref={cityInputRef} />
+        {!formValidity.city && <p>please enter valid city!</p>}
+      </div>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
+    </form>
+  );
+};
+
+export default Checkout;
---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://my-react-app-b9752-default-rtdb.firebaseio.com/meales.json"
+      );
+
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <p className={classes.mealsLoding}>is loading...</p>;
+  }
+
+  if (error) {
+    return <p className={classes.mealsError}>{error}</p>;
+  }
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  console.log(meals);
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
